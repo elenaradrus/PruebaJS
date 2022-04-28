a7e8a2979fac6fb5c19374448674c7b09bca476d
Elena Radu <lena@MacBook-Air-de-Elena.local>
2022-04-28 13:31:33 +0200

    acabado

--- script.js
+++ script.js
@@ -6,24 +6,29 @@
 const iniciar = prompt("Escribe un 1 o un 2 para elegir opción");
 
 
-while (iniciar === 1) {
-    const firstNum = parseFloat(prompt("Escribe un número"));
-    const secondNum = parseFloat(promt("Escribe un segundo número"));
+do {
+    const firstNum = prompt("Escribe un número");
+    const secondNum = promt("Escribe un segundo número");
 
-    if(firstNum % 2 === 0 && secondNum % 2 === 0) {
-        alert("Ambos números son pares");
-    } else if (firstNum % 2 === 0 && secondNum % 2 !== 0) {
-        alert("Solo el primer número es par");
-    } else if (firstNum % 2 !== 0 && secondNum % 2 === 0) {
-        alert("Solo el segundo númer es par");
-    } else (firstNum % 2 !== 0 && secondNum % 2 !== 0); {
-        alert("Ningún número es par");
+    if(firstNum % 2 === 0 && secondNum % 2 === 0 && firstNum % 7 === 0 && secondNum % 7 === 0) {
+        alert("Ambos números son pares y multiplos de 7");
+    } else if (firstNum % 2 === 0 && secondNum % 2 !== 0 && firstNum % 7 === 0 && secondNum % 7 !== 0) {
+        alert("Solo el primer número es par y multiplo de 7");
+    } else if (firstNum % 2 !== 0 && secondNum % 2 === 0 && firstNum % 7 !== 0 && secondNum % 7 === 0) {
+        alert("Solo el segundo número es par y multiplo de 7");
+    } else (firstNum % 2 !== 0 && secondNum % 2 !== 0 && firstNum % 7 !== 0 && secondNum % 7 !== 0); {
+        alert("Ningún número es par o multiplo de 7");
     }
-}
+} while (iniciar == 1) 
+
 
 while (iniciar == 2) {
     const frase = prompt("Escribe una frase").toLocaleLowerCase();
+
     for (let i = 0; i < frase.length;  i++) {
+        if (frase[0] == "a" || frase[i].length - 1 == "a");
         alert("Tú frase empieza o acaba por A");
     }
-} 
+}   
+    
+ 
